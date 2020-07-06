9677cddb6767b867cfbf53f3f4aa555e3d0c6e91
Walter Joy IV <waltoids@users.noreply.github.com>
2020-07-06 18:23:54 -0400

    fixed an error writing to db.json

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 module.exports = function(app) {
     app.get('/api/notes', function(req, res) {
         try {
-           const db_response = fs.readFileSync('../db/db.json', 'utf8');
+           const db_response = fs.readFileSync('./db/db.json', 'utf8');
 
            return res.json(JSON.parse(db_response));
         }catch (error) {
@@ -18,13 +18,13 @@ module.exports = function(app) {
         const { body } = req;
 
         try {
-            const db_response = fs.readFileSync('../db/db.json', 'utf8');
+            const db_response = fs.readFileSync('./db/db.json', 'utf8');
             const notes = JSON.parse(db_response);
 
             body.id = notes.length;
             notes.push(body);
 
-            fs.writeFileSync('../db/db.json', JSON.stringify(notes));
+            fs.writeFileSync('./db/db.json', JSON.stringify(notes));
 
             return res.json(notes);
         }catch(error) {
@@ -34,16 +34,16 @@ module.exports = function(app) {
     });
 
     app.delete('/api/notes/:id', function(req, res) {
-        const { id } = req.params;
+        const { id } = req.params.id;
         try {
-            const db_response = fs.readFileSync('../db/db.json', 'utf8');
+            const db_response = fs.readFileSync('./db/db.json', 'utf8');
 
             const notes = JSON.parse(db_response);
             const filterNotes = notes.filter(function(note) {
-                if (note.id !==id) return note;
+                if (note.id !== id) return note;
             });
 
-            fs.writeFileSync('../db/db.json', JSON.stringify(filterNotes));
+            fs.writeFileSync('./db/db.json', JSON.stringify(filterNotes));
 
             return res.json(filterNotes);
 
